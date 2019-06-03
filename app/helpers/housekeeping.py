3bcb2bd7c6e4aaf0d14794b8728af0eedff3a1ee
Daan Raman <draman@nviso.be>
2019-06-03 16:33:42 +0200

    improve waiting mechanism in housekeeping threading

--- app/helpers/housekeeping.py
+++ app/helpers/housekeeping.py
@@ -24,6 +24,7 @@ class HousekeepingJob(threading.Thread):
                 settings.reload_configuration_files()  # reload configuration file, in case new whitelisted items were added by the analyst, they should be processed!
 
                 try:
+                    logging.logger.info("housekeeping - going to remove all whitelisted outliers")
                     total_docs_whitelisted = es.remove_all_whitelisted_outliers()
 
                     if total_docs_whitelisted > 0:
@@ -36,6 +37,6 @@ class HousekeepingJob(threading.Thread):
                     logging.logger.error(traceback.format_exc())
 
             logging.logger.info("housekeeping - finished round of cleaning whitelisted items, going to sleep %i seconds", housekeeping_interval_seconds)
-            time.sleep(int(housekeeping_interval_seconds))
+            self.shutdown_flag.wait(int(housekeeping_interval_seconds))
 
         logging.logger.info('housekeeping thread #%s stopped' % self.ident)
