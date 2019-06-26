c76ebbc04100399f8c12df2aab2534aec0a3b40f
Detobel36 <detobel36@phoenix-rebirth.fr>
2019-06-26 10:39:13 +0200

    Fix PEP8 line break

--- app/analyzers/simplequery.py
+++ app/analyzers/simplequery.py
@@ -40,12 +40,15 @@ class SimplequeryAnalyzer(Analyzer):
             query["filter"] = [exclude_hits_filter]
 
         self.total_events = es.count_documents(search_query=query)
-        logging.print_analysis_intro(event_type="evaluating " + self.config_section_name, total_events=self.total_events)
+        logging.print_analysis_intro(event_type="evaluating " + self.config_section_name,
+                                     total_events=self.total_events)
 
-        logging.init_ticker(total_steps=self.total_events, desc=self.model_name + " - evaluating " + self.model_type + " model")
+        logging.init_ticker(total_steps=self.total_events, desc=self.model_name + " - evaluating " + self.model_type + \
+                                                                " model")
         for doc in es.scan(search_query=query):
             logging.tick()
-            fields = es.extract_fields_from_document(doc, extract_derived_fields=self.model_settings["use_derived_fields"])
+            fields = es.extract_fields_from_document(doc,
+                                                     extract_derived_fields=self.model_settings["use_derived_fields"])
             self.process_outlier(fields, doc)
 
         self.print_analysis_summary()
