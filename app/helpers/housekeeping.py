e056d5e69a1132e6ac6a8cedf332000176b12ae5
Detobel36 <detobel36@phoenix-rebirth.fr>
2019-07-10 11:33:15 +0200

    Update housekeeping to use file modification (and not time)

--- app/helpers/housekeeping.py
+++ app/helpers/housekeeping.py
@@ -2,12 +2,17 @@ import threading
 import traceback
 
 from helpers.singletons import logging, settings, es
+from helpers.watchers import FileModificationWatcher
 
 
 class HousekeepingJob(threading.Thread):
 
     def __init__(self):
         threading.Thread.__init__(self)
+
+        file_mod_watcher = FileModificationWatcher()
+        file_mod_watcher.add_files(settings.args.config)
+        self.file_mod_watcher = file_mod_watcher
 
         # The shutdown_flag is a threading.Event object that
         # indicates whether the thread should be terminated.
@@ -18,26 +23,28 @@ class HousekeepingJob(threading.Thread):
 
         # Remove all existing whitelisted items if needed
         while not self.shutdown_flag.is_set():
-            # Read in the setting again from settings, as it could have changed in the meantime
-            housekeeping_interval_seconds = settings.config.getint("general", "housekeeping_interval_seconds")
 
-            if settings.config.getboolean("general", "es_wipe_all_whitelisted_outliers"):
-                settings.reload_configuration_files()  # reload configuration file, in case new whitelisted items were added by the analyst, they should be processed!
+            if len(self.file_mod_watcher.files_changed()) > 0:
+                # reload configuration file, in case new whitelisted items were added by the analyst, they
+                # should be processed!
+                settings.reload_configuration_files()
 
-                try:
-                    logging.logger.info("housekeeping - going to remove all whitelisted outliers")
-                    total_docs_whitelisted = es.remove_all_whitelisted_outliers()
+                if settings.config.getboolean("general", "es_wipe_all_whitelisted_outliers"):
+                    try:
+                        logging.logger.info("housekeeping - going to remove all whitelisted outliers")
+                        total_docs_whitelisted = es.remove_all_whitelisted_outliers()
 
-                    if total_docs_whitelisted > 0:
-                        logging.logger.info("housekeeping - total whitelisted documents cleared from outliers: " + str(total_docs_whitelisted))
-                    else:
-                        logging.logger.info("housekeeping - whitelist did not remove any outliers")
+                        if total_docs_whitelisted > 0:
+                            logging.logger.info(
+                                "housekeeping - total whitelisted documents cleared from outliers: " + str(
+                                    total_docs_whitelisted))
+                        else:
+                            logging.logger.info("housekeeping - whitelist did not remove any outliers")
 
-                except Exception:
-                    logging.logger.error("housekeeping - something went removing whitelisted outliers")
-                    logging.logger.error(traceback.format_exc())
+                    except Exception:
+                        logging.logger.error("housekeeping - something went removing whitelisted outliers")
+                        logging.logger.error(traceback.format_exc())
 
-                logging.logger.info("housekeeping - finished round of cleaning whitelisted items, going to sleep %i seconds", housekeeping_interval_seconds)
-            self.shutdown_flag.wait(int(housekeeping_interval_seconds))
+                    logging.logger.info("housekeeping - finished round of cleaning whitelisted items")
 
         logging.logger.info('housekeeping thread #%s stopped' % self.ident)
