3c0759a6087b4bafb767f8cc961ef829061ce390
Detobel36 <detobel36@phoenix-rebirth.fr>
2019-07-11 11:18:52 +0200

    Begin test for Metrics

---
+++ app/tests/unit_tests/test_analyzer_metrics.py
@@ -0,0 +1,66 @@
+import unittest
+
+from tests.unit_tests.test_stubs.test_stub_es import TestStubEs
+from helpers.singletons import settings
+from analyzers.metrics import MetricsAnalyzer
+
+from collections import defaultdict
+
+
+class TestMetricsAnalyzer(unittest.TestCase):
+
+    def setUp(self):
+        self.test_es = TestStubEs()
+
+    def tearDown(self):
+        # restore the default configuration file so we don't influence other unit tests that use the settings singleton
+        settings.process_configuration_files("/defaults/outliers.conf")
+        settings.process_arguments()
+        self.test_es.restore_es()
+
+    def _preperate_data_terms(self):
+        eval_metrics_array = defaultdict()
+        # "random" value
+        aggregator_value = "key"
+        target_value = "test"
+        metrics_value = "metric_value"
+        observations = {'a': 1, 'test': 'ok'}
+        doc = {'source': 'this', 'target': 12}
+        return eval_metrics_array, aggregator_value, target_value, metrics_value, observations, doc
+
+    def test_add_metric_to_batch_empty(self):
+        eval_metrics_array = defaultdict()
+        aggregator_value = ""
+        target_value = ""
+        metrics_value = ""
+        observations = {}
+        doc = {}
+        # # Create expected result
+        observations["target"] = [target_value]
+        observations["aggregator"] = [aggregator_value]
+        expected_eval_terms = defaultdict()
+        expected_eval_terms[aggregator_value] = defaultdict(list)
+        expected_eval_terms[aggregator_value]["metrics"] = [metrics_value]
+        expected_eval_terms[aggregator_value]["observations"] = [observations]
+        expected_eval_terms[aggregator_value]["raw_docs"] = [doc]
+
+        result = MetricsAnalyzer.add_metric_to_batch(eval_metrics_array, aggregator_value, target_value, metrics_value,
+                                                     observations, doc)
+        self.assertEqual(result, expected_eval_terms)
+
+    def test_add_term_to_batch_no_modification(self):
+        eval_metrics_array, aggregator_value, target_value, metrics_value, observations, doc = \
+            self._preperate_data_terms()
+
+        # # Create expected result
+        observations["target"] = [target_value]
+        observations["aggregator"] = [aggregator_value]
+        expected_eval_terms = defaultdict()
+        expected_eval_terms[aggregator_value] = defaultdict(list)
+        expected_eval_terms[aggregator_value]["metrics"] = [metrics_value]
+        expected_eval_terms[aggregator_value]["observations"] = [observations]
+        expected_eval_terms[aggregator_value]["raw_docs"] = [doc]
+
+        result = MetricsAnalyzer.add_metric_to_batch(eval_metrics_array, aggregator_value, target_value, metrics_value,
+                                                     observations, doc)
+        self.assertEqual(result, expected_eval_terms)
