3a3a9cfe0c078e08266c82b169a08f6240187f66
Detobel36 <detobel36@phoenix-rebirth.fr>
2019-07-11 15:09:30 +0200

    Remove useless function

--- app/helpers/logging.py
+++ app/helpers/logging.py
@@ -66,6 +66,3 @@ class Logging:
     def print_generic_intro(self, title):
         self.logger.info("")
         self.logger.info("===== " + title + " =====")
-
-    def format_number(self, number):
-        return '{0:,d}'.format(number).replace(',', ' ')
