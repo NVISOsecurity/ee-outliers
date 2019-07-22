4f3d0f76d15e891583166ad66431782b8e7c64e4
Daan Raman <draman@nviso.be>
2019-07-22 17:38:21 +0200

    split up outliers.py into several functions

--- app/outliers.py
+++ app/outliers.py
@@ -23,47 +23,174 @@ from analyzers.terms import TermsAnalyzer
 from analyzers.beaconing import BeaconingAnalyzer
 from analyzers.word2vec import Word2VecAnalyzer
 
+
 ##############
 # Entrypoint #
 ##############
-if os.environ.get("SENTRY_SDK_URL"):
-    import sentry_sdk
-    sentry_sdk.init(os.environ.get("SENTRY_SDK_URL"))
+def run_outliers():
+    # Run modes
 
-if settings.args.run_mode == "tests":
+    # if running in test mode, we just want to run the tests and exit as quick as possible.
+    # no need to set up other things like logging, which should happen afterwards.
+    if settings.args.run_mode == "tests":
+        test_filename = 'test_*.py'
+        test_directory = '/app/tests/unit_tests'
 
-    test_filename = 'test_*.py'
-    test_directory = '/app/tests/unit_tests'
+        suite = unittest.TestLoader().discover(test_directory, pattern=test_filename)
+        unittest.TextTestRunner(verbosity=settings.config.getint("general", "log_verbosity")).run(suite)
+        sys.exit()
 
-    suite = unittest.TestLoader().discover(test_directory, pattern=test_filename)
-    unittest.TextTestRunner(verbosity=settings.config.getint("general", "log_verbosity")).run(suite)
-    sys.exit()
+    # at this point, we know we are not running tests, so we should set up logging,
+    # parse the configuration files, etc.
+    setup_logging()
+    print_intro()
 
-# Configuration for which we need access to both settings and logging singletons should happen here
-logging.verbosity = settings.config.getint("general", "log_verbosity")
-logging.logger.setLevel(settings.config.get("general", "log_level"))
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = settings.config.get("machine_learning", "tensorflow_log_level")
+    # everything has been setup correctly, we can now start analysis in the correct run mode
+    if settings.args.run_mode == "daemon":
+        run_daemon_mode()
 
-# Log Handlers
-LOG_FILE = settings.config.get("general", "log_file")
+    if settings.args.run_mode == "interactive":
+        run_interactive_mode()
 
-if os.path.exists(os.path.dirname(LOG_FILE)):
-    logging.add_file_handler(LOG_FILE)
-else:
-    logging.logger.warning("log directory for log file %s does not exist, check your settings! Only logging to stdout.",
-                           LOG_FILE)
 
-logging.logger.info("outliers.py started - contact: research@nviso.be")
-logging.logger.info("run mode: " + settings.args.run_mode)
+def setup_logging():
+    if os.environ.get("SENTRY_SDK_URL"):
+        import sentry_sdk
+        sentry_sdk.init(os.environ.get("SENTRY_SDK_URL"))
 
-logging.print_generic_intro("initializing")
-logging.logger.info("loaded " + str(len(settings.loaded_config_paths)) + " configuration files")
+    # Configuration for which we need access to both settings and logging singletons should happen here
+    logging.verbosity = settings.config.getint("general", "log_verbosity")
+    logging.logger.setLevel(settings.config.get("general", "log_level"))
+    os.environ['TF_CPP_MIN_LOG_LEVEL'] = settings.config.get("machine_learning", "tensorflow_log_level")
 
-if settings.failed_config_paths:
-    logging.logger.warning("failed to load " + str(len(settings.failed_config_paths)) + " configuration files")
+    # Log Handlers
+    log_file = settings.config.get("general", "log_file")
 
-    for failed_config_path in settings.failed_config_paths:
-        logging.logger.warning("failed to load " + str(failed_config_path))
+    if os.path.exists(os.path.dirname(log_file)):
+        logging.add_file_handler(log_file)
+    else:
+        logging.logger.warning("log directory for log file %s does not exist, check your settings! Only logging to stdout.",
+                               log_file)
+
+
+def print_intro():
+    logging.logger.info("outliers.py started - contact: research@nviso.be")
+    logging.logger.info("run mode: " + settings.args.run_mode)
+
+    logging.print_generic_intro("initializing")
+    logging.logger.info("loaded " + str(len(settings.loaded_config_paths)) + " configuration files")
+
+    if settings.failed_config_paths:
+        logging.logger.warning("failed to load " + str(len(settings.failed_config_paths)) + " configuration files")
+
+        for failed_config_path in settings.failed_config_paths:
+            logging.logger.warning("failed to load " + str(failed_config_path))
+
+
+def run_daemon_mode():
+    # In daemon mode, we also want to monitor the configuration file for changes.
+    # In case of a change, we need to make sure that we are using this new configuration file
+    for config_file in settings.args.config:
+        logging.logger.info("monitoring configuration file " + config_file + " for changes")
+
+    file_mod_watcher = FileModificationWatcher()
+    file_mod_watcher.add_files(settings.args.config)
+
+    # Initialize Elasticsearch connection
+    es.init_connection()
+
+    # Create housekeeping job, don't start it yet
+    housekeeping_job = HousekeepingJob()
+
+    num_runs = 0
+    first_run = True
+    run_succeeded_without_errors = None
+
+    while True:
+        num_runs += 1
+        next_run = None
+        should_schedule_next_run = False
+
+        while (next_run is None or datetime.now() < next_run) and first_run is False and \
+                run_succeeded_without_errors is True:
+            if next_run is None:
+                should_schedule_next_run = True
+
+            # Check for configuration file changes and load them in case it's needed
+            if file_mod_watcher.files_changed():
+                logging.logger.info("configuration file changed, reloading")
+                settings.process_configuration_files()
+                should_schedule_next_run = True
+
+            if should_schedule_next_run:
+                next_run = croniter(settings.config.get("daemon", "schedule"), datetime.now()).get_next(datetime)
+                logging.logger.info("next run scheduled on {0:%Y-%m-%d %H:%M:%S}".format(next_run))
+                should_schedule_next_run = False
+
+            time.sleep(5)
+
+        settings.process_configuration_files()  # Refresh settings
+
+        if first_run:
+            first_run = False
+            logging.logger.info("first run, so we will start immediately - after this, we will respect the cron " +
+                                "schedule defined in the configuration file")
+
+            # Wipe all existing outliers if needed
+            if settings.config.getboolean("general", "es_wipe_all_existing_outliers"):
+                logging.logger.info("wiping all existing outliers on first run")
+                es.remove_all_outliers()
+        else:
+            # Make sure we are still connected to Elasticsearch before analyzing, in case something went wrong with
+            # the connection in between runs
+            es.init_connection()
+
+        # Make sure housekeeping is up and running
+        if not housekeeping_job.is_alive():
+            housekeeping_job.start()
+
+        # Perform analysis
+        logging.print_generic_intro("starting outlier detection")
+        analyzed_models = perform_analysis()
+        print_analysis_summary(analyzed_models)
+
+        errored_models = [analyzer for analyzer in analyzed_models if analyzer.unknown_error_analysis]
+
+        # Check the result of the analysis
+        if len(errored_models) > 0:
+            run_succeeded_without_errors = False
+            logging.logger.warning("ran into errors while analyzing use cases - not going to wait for the cron " +
+                                   "schedule, we just start analyzing again after sleeping for a minute first")
+            time.sleep(60)
+        else:
+            run_succeeded_without_errors = True
+
+        logging.print_generic_intro("finished performing outlier detection")
+
+
+def run_interactive_mode():
+    es.init_connection()
+
+    if settings.config.getboolean("general", "es_wipe_all_existing_outliers"):
+        es.remove_all_outliers()
+
+    # Make sure housekeeping is up and running
+    housekeeping_job = HousekeepingJob()
+    housekeeping_job.start()
+
+    try:
+        analyzed_models = perform_analysis()
+        print_analysis_summary(analyzed_models)
+    except KeyboardInterrupt:
+        logging.logger.info("keyboard interrupt received, stopping housekeeping thread")
+    except Exception:
+        logging.logger.error(traceback.format_exc())
+    finally:
+        logging.logger.info("asking housekeeping jobs to shutdown after finishing")
+        housekeeping_job.shutdown_flag.set()
+        housekeeping_job.join()
+
+    logging.logger.info("finished performing outlier detection")
 
 
 def perform_analysis():
@@ -149,122 +276,19 @@ def print_analysis_summary(analyzed_models):
 
     analysis_times = list()
     for _analyzer in completed_models_with_events:
-            analysis_times.append(_analyzer.get_analysis_time())
+        analysis_times.append(_analyzer.get_analysis_time())
 
-    if len(completed_models):
+    if completed_models:
         logging.logger.info("")
         logging.logger.info("total analysis time: " + helpers.utils.strfdelta(tdelta=int(np.sum(analysis_times)), inputtype="seconds", fmt='{D}d {H}h {M}m {S}s'))
         logging.logger.info("average analysis time: " + helpers.utils.strfdelta(tdelta=int(np.average(analysis_times)), inputtype="seconds", fmt='{D}d {H}h {M}m {S}s'))
 
-    if len(analyzed_models) == 0:
+    if not analyzed_models:
         logging.logger.warning("no use cases were analyzed. are you sure your configuration file contains use " +
                                "cases, which are enabled?")
 
     logging.logger.info("============================")
 
 
-# Run modes
-if settings.args.run_mode == "daemon":
-    # In daemon mode, we also want to monitor the configuration file for changes.
-    # In case of a change, we need to make sure that we are using this new configuration file
-    for config_file in settings.args.config:
-        logging.logger.info("monitoring configuration file " + config_file + " for changes")
-
-    file_mod_watcher = FileModificationWatcher()
-    file_mod_watcher.add_files(settings.args.config)
-
-    # Initialize Elasticsearch connection
-    es.init_connection()
-
-    # Create housekeeping job, don't start it yet
-    housekeeping_job = HousekeepingJob()
-
-    num_runs = 0
-    first_run = True
-    run_succeeded_without_errors = None
-
-    while True:
-        num_runs += 1
-        next_run = None
-        should_schedule_next_run = False
-
-        while (next_run is None or datetime.now() < next_run) and first_run is False and \
-                run_succeeded_without_errors is True:
-            if next_run is None:
-                should_schedule_next_run = True
-
-            # Check for configuration file changes and load them in case it's needed
-            if len(file_mod_watcher.files_changed()) > 0:
-                logging.logger.info("configuration file changed, reloading")
-                settings.process_configuration_files()
-                should_schedule_next_run = True
-
-            if should_schedule_next_run:
-                next_run = croniter(settings.config.get("daemon", "schedule"), datetime.now()).get_next(datetime)
-                logging.logger.info("next run scheduled on {0:%Y-%m-%d %H:%M:%S}".format(next_run))
-                should_schedule_next_run = False
-
-            time.sleep(5)
-
-        settings.process_configuration_files()  # Refresh settings
-
-        if first_run:
-            first_run = False
-            logging.logger.info("first run, so we will start immediately - after this, we will respect the cron " +
-                                "schedule defined in the configuration file")
-
-            # Wipe all existing outliers if needed
-            if settings.config.getboolean("general", "es_wipe_all_existing_outliers"):
-                logging.logger.info("wiping all existing outliers on first run")
-                es.remove_all_outliers()
-        else:
-            # Make sure we are still connected to Elasticsearch before analyzing, in case something went wrong with
-            # the connection in between runs
-            es.init_connection()
-
-        # Make sure housekeeping is up and running
-        if not housekeeping_job.is_alive():
-            housekeeping_job.start()
-
-        # Perform analysis
-        logging.print_generic_intro("starting outlier detection")
-        analyzed_models = perform_analysis()
-        print_analysis_summary(analyzed_models)
-
-        errored_models = [analyzer for analyzer in analyzed_models if analyzer.unknown_error_analysis]
-
-        # Check the result of the analysis
-        if len(errored_models) > 0:
-            run_succeeded_without_errors = False
-            logging.logger.warning("ran into errors while analyzing use cases - not going to wait for the cron " +
-                                   "schedule, we just start analyzing again after sleeping for a minute first")
-            time.sleep(60)
-        else:
-            run_succeeded_without_errors = True
-
-        logging.print_generic_intro("finished performing outlier detection")
-
-
-elif settings.args.run_mode == "interactive":
-    es.init_connection()
-
-    if settings.config.getboolean("general", "es_wipe_all_existing_outliers"):
-        es.remove_all_outliers()
-
-    # Make sure housekeeping is up and running
-    housekeeping_job = HousekeepingJob()
-    housekeeping_job.start()
-
-    try:
-        analyzed_models  = perform_analysis()
-        print_analysis_summary(analyzed_models)
-    except KeyboardInterrupt:
-        logging.logger.info("keyboard interrupt received, stopping housekeeping thread")
-    except Exception:
-        logging.logger.error(traceback.format_exc())
-    finally:
-        logging.logger.info("asking housekeeping jobs to shutdown after finishing")
-        housekeeping_job.shutdown_flag.set()
-        housekeeping_job.join()
-
-    logging.logger.info("finished performing outlier detection")
+if __name__ == '__main__':
+    run_outliers()
