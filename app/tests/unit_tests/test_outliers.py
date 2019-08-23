a896ab0404dda6c33d19b4c98fcedba658a94f54
Detobel36 <detobel36@phoenix-rebirth.fr>
2019-08-23 16:36:45 +0200

    Fix tests

--- app/tests/unit_tests/test_outliers.py
+++ app/tests/unit_tests/test_outliers.py
@@ -177,11 +177,11 @@ class TestOutlierOperations(unittest.TestCase):
         test_outlier = Outlier(outlier_type="dummy type", outlier_reason="dummy reason",
                                outlier_summary="dummy summary", doc=orig_doc)
 
-        self.test_settings.change_configuration_path("/app/tests/unit_tests/files/whitelist_tests_06.conf")
+        self.test_settings.change_configuration_path("/app/tests/unit_tests/files/whitelist_tests_06_with_general.conf")
         self.assertTrue(test_outlier.is_whitelisted())
 
     def test_single_regex_not_to_match_in_doc_with_outlier(self):
-        self.test_settings.change_configuration_path("/app/tests/unit_tests/files/whitelist_tests_07.conf")
+        self.test_settings.change_configuration_path("/app/tests/unit_tests/files/whitelist_tests_07_with_general.conf")
         orig_doc = copy.deepcopy(doc_with_outlier_test_file)
         test_outlier = Outlier(outlier_type="dummy type", outlier_reason="dummy reason",
                                outlier_summary="dummy summary", doc=orig_doc)
