b2acc63344a853691e5183b6dbf0d48ede06e1b3
mroberti <robertimaximilien@gmail.com>
2020-06-19 15:45:03 +0200

    use fallback option when use_derived_fields is retrieved

--- app/helpers/analyzer.py
+++ app/helpers/analyzer.py
@@ -94,9 +94,7 @@ class Analyzer(abc.ABC):
         except NoOptionError:
             model_settings["should_notify"] = False
 
-        model_settings["use_derived_fields"] = self.config_section.getboolean("use_derived_fields")
-        if model_settings["use_derived_fields"] is None:
-            model_settings["use_derived_fields"] = False
+        model_settings["use_derived_fields"] = self.config_section.getboolean("use_derived_fields", fallback=False)
 
         model_settings["es_index"] = self.config_section.get("es_index")
         if not model_settings["es_index"]:
