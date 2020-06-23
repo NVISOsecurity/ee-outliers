b0486724677288104019b01ff1410a7e4577f535
mroberti <robertimaximilien@gmail.com>
2020-06-23 12:48:32 +0200

    additional tests to simplequery non chronologicaly vs chronologicaly

--- app/analyzers/simplequery.py
+++ app/analyzers/simplequery.py
@@ -2,7 +2,6 @@ from helpers.singletons import settings, es, logging
 from helpers.analyzer import Analyzer
 import re
 from configparser import NoSectionError, NoOptionError
-import datetime as dt
 
 
 class SimplequeryAnalyzer(Analyzer):
@@ -14,6 +13,7 @@ class SimplequeryAnalyzer(Analyzer):
         """
         Override method from Analyzer
         """
+        self.model_settings["process_documents_chronologically"] = self.config_section.getboolean("process_documents_chronologically")
         self.model_settings["highlight_match"] = self.config_section.getboolean("highlight_match")
         if self.model_settings["highlight_match"] is None:
             try:
@@ -54,26 +54,10 @@ class SimplequeryAnalyzer(Analyzer):
             query["filter"].append(exclude_hits_filter)
         else:
             query["filter"] = [exclude_hits_filter]
-        # TEST 1
-        logging.logger.debug("process_documents_chronologically set to: "
-                             + str(self.model_settings["process_documents_chronologically"]))
-        start_time = dt.datetime.today().timestamp()
-        self.total_events, documents = es.count_and_scan_documents(index=self.model_settings["es_index"],
-                                                                   search_query=query,
-                                                                   model_settings=self.model_settings)
-        diff_time = float(dt.datetime.today().timestamp() - start_time)
-        logging.logger.debug("Time count_and_scan_documents: " + str(diff_time))
 
-        # TEST 2
-        self.model_settings["process_documents_chronologically"] = True
-        logging.logger.debug("process_documents_chronologically set to: "
-                             + str(self.model_settings["process_documents_chronologically"]))
-        start_time = dt.datetime.today().timestamp()
         self.total_events, documents = es.count_and_scan_documents(index=self.model_settings["es_index"],
                                                                    search_query=query,
                                                                    model_settings=self.model_settings)
-        diff_time = float(dt.datetime.today().timestamp() - start_time)
-        logging.logger.debug("Time count_and_scan_documents: " + str(diff_time))
 
         self.print_analysis_intro(event_type="evaluating " + self.model_type + "_" + self.model_name,
                                   total_events=self.total_events)
