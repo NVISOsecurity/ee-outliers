c75c4f0bf1c99ee0a404e578fadce5adf5fbefa3
mroberti <robertimaximilien@gmail.com>
2020-07-16 18:17:52 +0200

    test

--- app/analyzers/sudden_appearance.py
+++ app/analyzers/sudden_appearance.py
@@ -131,7 +131,7 @@ class SuddenAppearanceAnalyzer(Analyzer):
         # Loop over the aggregations
         for aggregator_bucket in aggregator_buckets:
             target_buckets = aggregator_bucket["target"]["buckets"]
-            logging.logger.debug(aggregator_bucket["key"])
+            # logging.logger.debug(aggregator_bucket["key"])
             # Loop over the documents in aggregation
             for doc in target_buckets:
                 self.num_event_proc += doc["doc_count"]
