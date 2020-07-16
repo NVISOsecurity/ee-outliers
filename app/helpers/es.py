d996841f5737b88eeebd88f805240082a1185c4d
mroberti <robertimaximilien@gmail.com>
2020-07-16 18:13:35 +0200

    test

--- app/helpers/es.py
+++ app/helpers/es.py
@@ -760,7 +760,7 @@ def build_script(field_list):
     :param field_list: list of event field
     :return script:  painless format script
     """
-    script = "doc['" + field_list[0] + ".keyword'].value"
+    script = "doc['" + field_list[0] + "'].value"
     for i in range(1, len(field_list)):
-        script += "+ ' - ' + doc['" + field_list[i] + ".keyword'].value"
+        script += "+ ' - ' + doc['" + field_list[i] + "'].value"
     return script
