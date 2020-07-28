51e136e96c620ef841890a5a771c460a878eae35
mroberti <robertimaximilien@gmail.com>
2020-07-28 15:02:02 +0200

    For test

--- app/analyzers/sudden_appearance.py
+++ app/analyzers/sudden_appearance.py
@@ -141,7 +141,7 @@ class SuddenAppearanceAnalyzer(Analyzer):
                     outlier = self.create_outlier(fields,
                                                   raw_doc,
                                                   extra_outlier_information=extra_outlier_information)
-                    # logging.logger.debug(outlier)
+                    logging.logger.debug(outlier)
                     self.process_outlier(outlier)
 
         logging.tick(self.num_event_proc)
