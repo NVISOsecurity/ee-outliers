5c33a0a020508a1aa7fb0419058005e9e1069faa
mroberti <robertimaximilien@gmail.com>
2020-08-17 15:10:34 +0200

    modification for test on production environment

--- app/helpers/analyzer.py
+++ app/helpers/analyzer.py
@@ -225,8 +225,6 @@ class Analyzer(abc.ABC):
         """
         self.total_outliers += 1
         self.outlier_summaries.add(outlier.outlier_dict["summary"])
-        logging.logger.debug("regerxps withelist")
-        logging.logger.debug(self.model_whitelist_regexps)
         if outlier.is_whitelisted(self.model_whitelist_literals, self.model_whitelist_regexps):
             logging.logger.debug("outlier is white listed!")
             if settings.print_outliers_to_console:
